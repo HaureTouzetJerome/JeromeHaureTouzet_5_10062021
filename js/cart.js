55a2b6d3ec3f25520292fe5ab9d956854d62ac73
HaureTouzetJerome <hauretouzetjerome@gmail.com>
2021-07-02 09:14:04 +0200

    ADD - supression d'un produits du panier

--- js/cart.js
+++ js/cart.js
@@ -8,7 +8,7 @@ if(productsLocalStorage){
 const mainContainer = document.querySelector("#main-item");
 const titleCart = document.createElement("h2");
 
-if (productsLocalStorage){
+if (productsLocalStorage && productsCart.length != 0){
     titleCart.innerHTML = "Voici le contenu de votre panier";
 
     const productCart = document.createElement("div");
@@ -24,7 +24,7 @@ if (productsLocalStorage){
                                  <div>${productsCart[i].color}</div>
                                  <div>Quantité: ${productsCart[i].quantity}</div>
                                  <div>${productsCart[i].price} €</div>
-                                 <div>Supprimer l'article</div>`;
+                                 <button class="btn-deleteProduct"> Supprimer l'article </button>`;
         productCart.appendChild(infoProduct);
     }
 
@@ -42,4 +42,18 @@ if (productsLocalStorage){
 else{
     mainContainer.appendChild(titleCart);
     titleCart.innerHTML = "Votre panier est vide";
+}
+
+let listButtons = document.querySelectorAll(".btn-deleteProduct");
+
+for(let i = 0; i < listButtons.length; i++){
+    listButtons[i].addEventListener("click", (event)=>{
+        event.preventDefault();
+
+        let idProductToDelete = productsCart[i].id;
+        index = productsCart.findIndex(element => element.id === idProductToDelete);
+        productsCart.splice(index, 1);
+        localStorage.setItem("productsCart", JSON.stringify(productsCart));
+        document.location.reload();
+    })
 }
